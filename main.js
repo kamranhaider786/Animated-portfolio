9c70065764a67c095b1d7b443e96637181134b27
kamranhaider786 <102785079+kamranhaider786@users.noreply.github.com>
2022-11-07 01:50:26 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+		var typingEffect = new Typed(".multiText",{
+			strings : ["Coder","Developer","Designer","Sniper"],
+			loop : true,
+			typeSpeed : 100,
+			backSpeed: 80,
+			backDelay: 1500,
+		})
